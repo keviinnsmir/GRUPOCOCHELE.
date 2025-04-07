c043b9770e23523e058435a359a8ab3ca4f048e3
keviinnsmir <kevinquilumba026@gmail.com>
2025-04-07 08:54:08 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Función para activar o desactivar el menú en dispositivos móviles
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active'); // Añade o quita la clase 'active' para mostrar u ocultar el menú
-}
